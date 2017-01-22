9b8ce71d9affac5131f928624430e8709d6c274b
Joshua Sherbrooke <joshua@Joshuas-MacBook-Pro.local>
2017-01-22 07:34:34 -0800

    Some initial code for running Iridium SDB board. Not working state.

--- avionics.h
+++ avionics.h
@@ -1,1 +1,13 @@
-//avionics header 
+#define kPin_IRIDIUM_RX		18
+#define kPin_IRIDIUM_TX		19
+#define kPin_IRIDIUM_SLEEP  10
+
+class Avionics {
+public:
+	int initAvionics();
+	void demoCom(char message);
+private:
+	int initCom();
+}
+
+//factory class
